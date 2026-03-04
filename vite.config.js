785acf3e2913ecd330a702e200bfa4539d81d080
Juan Alberto Solis <jsolis@liderman.com>
2026-03-04 16:03:30 -0500

    hecho
    hecho

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    // Set base to your GitHub repository name for GitHub Pages
+    // Change 'ESTADOSUNIDOSWEB2' if your repo has a different name
+    base: '/ESTADOSUNIDOSWEB2/',
+})
